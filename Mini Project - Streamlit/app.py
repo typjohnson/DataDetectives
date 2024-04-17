35c9f4760823314dc999207cc006c6b1ecaf0526
typjohnson <121874260+typjohnson@users.noreply.github.com>
2024-04-17 19:30:34 -0400

    Add files via upload

--- Mini Project - Streamlit/app.py
+++ Mini Project - Streamlit/app.py
@@ -1,3 +1,4 @@
+#importing necessary libraries
 import streamlit as st
 import pandas as pd
 from ucimlrepo import fetch_ucirepo 
@@ -7,7 +8,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, classification_report
 from sklearn.ensemble import RandomForestClassifier
-
 import seaborn as sns
 import matplotlib.pyplot as plt
 
@@ -19,9 +19,10 @@ df = wine_quality.data.original
 
 
 
-#Page title
+#title 
 st.title('Wine Quality Viewer')
 
+#creating and naming the tabs of page
 tab1, tab2, tab3, tab4, tab5, tab6, tab7 = st.tabs(['Metadata', 'Data', 'Color', 'Quality', 'Scatter', 'Box', 'Classification'])
 with tab1:
     st.header('Metadata')
@@ -31,6 +32,7 @@ with tab1:
 
 with tab2: 
     st.header('Dataset Viewer')
+    #radio buttons to display different selections of the dataset
     columns = st.radio('Choose Dataset View:', ['Chemical', 'Alcohol', 'Quality', 'All'])
     if columns == 'Chemical':
         st.write(df[['color', 'fixed_acidity', 'volatile_acidity', 'citric_acid', 'chlorides',
@@ -45,10 +47,6 @@ with tab2:
     
 with tab3:
     st.header('Color Distribution')
-    # fig = plt.figure(figsize= (12,6))
-    # sns.countplot(x = 'color', data = df, color ='blue')
-    # st.pyplot(fig)
-
     wine_type_counts = df['color'].value_counts()
     fig, ax = plt.subplots()
     wine_type_counts.plot(kind='bar', ax=ax)
@@ -76,11 +74,13 @@ with tab5:
         'chlorides', 'free_sulfur_dioxide', 'total_sulfur_dioxide', 'density',
         'pH', 'sulphates', 'alcohol'
     ]
+    #using a for loop to iterate through variables and create a separate checkbox
     selected_options = {variable: False for variable in variables}
     for variable in variables:
         if st.checkbox(variable):
             selected_options[variable] = True
     
+    #checking which items are selected and creating plot
     selected_variables = [variable for variable, selected in selected_options.items() if selected]
     if len(selected_variables) == 2:
         fig, ax = plt.subplots()
@@ -97,11 +97,13 @@ with tab5:
 with tab6: 
     st.header('Box Plots')
     st.write('Choose which box plot to display')
+    #radio buttons that determine which column to plot
     columns = st.radio('Choose column to plot', ['fixed_acidity', 'volatile_acidity', 'citric_acid',
                                                    'residual_sugar', 'chlorides', 'free_sulfur_dioxide',
                                                      'total_sulfur', 'density', 'pH',
                                                        'sulphates', 'alcohol'], index = None)
     plt.figure(figsize=(8, 6))
+    #boxplot created by accessing currently selected radio button
     sns.boxplot(data=df, y=columns, orient='v')
     st.pyplot(plt)
     
@@ -118,6 +120,7 @@ with tab7:
     classifier = RandomForestClassifier()
     classifier.fit(X_train.values, y_train.values.ravel())
     y_pred = classifier.predict(X_test.values)
+    #creating the report and converting it to table format
     report = classification_report(y_test, y_pred,output_dict=True)
     report_df = pd.DataFrame(report).transpose()
     st.dataframe(report_df, width=1000)
@@ -143,10 +146,7 @@ with tab7:
    
    
 
-# data (as pandas dataframes) 
-X = wine_quality.data.features 
-y = wine_quality.data.targets 
-  
+
 
 
 
